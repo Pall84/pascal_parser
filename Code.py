5c48d1bb2d427a066f81846d995e9cb13577d545
palleymundsson <palle10@ru.is>
2012-11-21 16:52:55 +0000

    byrjað

--- Code.py
+++ Code.py
@@ -2,7 +2,5 @@ __author__ = 'palleymundsson'
 
 # A record for intermediate code */ private:
 
-# CodeOp m op; /* operator */
-# SymbolTableEntry* m arg1; /* arg1 is in symbol table */
-# SymbolTableEntry* m arg2; /* arg2 is in symbol table */
-# SymbolTableEntry* m result;/* result is in symbol table */
+
+
---
+++ CodeGenerator.py
@@ -0,0 +1,26 @@
+__author__ = 'palleymundsson'
+
+# CodeOp m op; /* operator */
+# SymbolTableEntry* m arg1; /* arg1 is in symbol table */
+# SymbolTableEntry* m arg2; /* arg2 is in symbol table */
+# SymbolTableEntry* m result;/* result is in symbol table */
+
+class Quadruple:
+    def __init__(self):
+        # operator
+        self.m_op = None
+        # arg1 is in symbol table
+        self.arg1 = None
+        # arg2 is in symbol table
+        self.arg2 = None
+        # result is in symbol table
+        self.result = None
+
+class QuadrupleList():
+    def __init__(self):
+        pass
+
+class Code:
+    def __init__(self):
+        pass
+
